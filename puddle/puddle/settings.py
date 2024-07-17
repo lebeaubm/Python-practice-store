9dbcc508ec549f4d38a48174b2b90f1e2a3fbe6f
Lebeau Billy <lebeaubm@yahoo.com~>
2024-07-17 14:14:59 -0700

    redo

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -39,7 +39,7 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
 
 # Database configuration
 DATABASES = {
-    'default': env.db(),
+    'default': env.db(default='sqlite:///db.sqlite3'),
 }
 
 
