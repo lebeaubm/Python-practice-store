6edd9715644c11e0233425b5f97566ba7508b736
Lebeau Billy <lebeaubm@yahoo.com~>
2024-07-17 12:20:46 -0700

    again 4

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -19,8 +19,17 @@ from django.urls import path
 
 from core.views import index,contact
 
+# from django.urls import path
+# from . import views
+
+
+app_name = 'core'
+
 urlpatterns = [
     path('', index,name='index'),
+
+# path('core/', include('core.urls', namespace='core')),
+
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
